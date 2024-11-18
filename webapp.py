a268baa5aac4b1d965b06b3c9548887b790aca8c
SebastianFabio <seabass.fabio@gmail.com>
2024-11-18 09:24:25 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,8 +1,9 @@
-import os
-from flask import Flask, url_for, render_template, request
+from flask import Flask, request, render_template, flash
+from markupsafe import Markup
 from flask import redirect
 from flask import session
-
+import os
+import time
 app = Flask(__name__)
 
 # In order to use "sessions",you need a "secret key".
@@ -20,24 +21,40 @@ def startOver():
     session.clear() #clears variable values and creates a new session
     return redirect('/') # url_for('renderMain') could be replaced with '/'
 
-@app.route('/page1',methods=['GET','POST'])
+@app.route('/page1')
 def renderPage1():
-    
+    session["start_time"] = time.time()
     return render_template('page1.html')
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    
+    if "answerOne" not in session:
+        session["answerOne"]=request.form['andes1']    
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    
+    if "answerTwo" not in session:
+        session["answerTwo"]=request.form['danube1']    
     return render_template('page3.html')
     
 @app.route('/results', methods=['GET','POST'])
 def renderPage4():
-    return render_template('results.html')
-    
+    if "answerThree" not in session:
+        session["answerThree"]=request.form['tashkent1'] 
+    scoreValue=get_score()
+    session["end_time"] = time.time()
+    time_taken = session["end_time"] - session["start_time"]
+    return render_template('results.html', finalScore=scoreValue, time_taken=time_taken)
+
+def get_score():
+    scoreVal=0
+    if session["answerOne"]=="Argentina" and "answerOne" in session:
+        scoreVal+=1
+    if session["answerTwo"]=="Bulgaria" and "answerTwo" in session:
+        scoreVal+=1
+    if session["answerThree"]=="Uzbekistan" and "answerThree" in session:
+        scoreVal+=1
+    return scoreVal
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
